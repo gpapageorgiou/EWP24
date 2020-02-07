8c9246d2bd28eadf41abf987224f5b52fb1ae1b4
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2020-02-07 11:08:07 +0100

    Interacts Q12

---
+++ R/Q12_interacts.R
@@ -0,0 +1,24 @@
+x <- seq(0, 20, length.out = 500)
+
+y1 <- 10 + 5*x
+y2 <- 20 + 5*x
+
+y3 <- 10 + 2.5*x
+y4 <- 40 + 5*x
+
+y5 <- 10 - 0.5*x
+y6 <- 80 - 0.5*x
+
+y7 <- 10 + 5*x
+y8 <- 30 - 2*x
+
+plotdat <- data.frame("x" = rep(x, 8), 
+                      "y" = c(y1, y2, y3, y4, y5, y6, y7, y8), 
+                      "pair" = rep(c(1, 2, 3, 4), each = 500*2), 
+                      "sex" = rep(rep(c("Females", "Males"), each = 500), 8))
+
+library(ggplot2)
+
+ggplot(plotdat) + 
+  geom_line(aes(x = x, y = y, color = sex)) + facet_wrap(~ pair) + 
+  ylab("Outcome") + xlab("Age")
